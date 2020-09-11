4af4ee50345f2ee2e2ce83bad1751aed2aaab425
thetayloredman <nutdriver716@gmail.com>
2020-09-10 18:22:02 -0700

    Core

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * enenmap
+ * By BadBoyHaloCat
+ * Enhanced Enmap | By BadBoyHaloCat | Extends Enmap by Evie.Codes
+ */
+
+// Modules
+const enmap = require('enmap');
+
+// Main class
+class enenmap extends enmap {
+
+}
+
+module.exports = enenmap;
