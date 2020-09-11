58d75cdec1dd3998ec5629a2d4b8458c97e2f4e7
WilsontheWolf <33164598+WilsontheWolf@users.noreply.github.com>
2020-09-10 22:34:39 -0600

    Made code WAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAY Cleaner

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ class EnEnmap extends Enmap {
     /**
      * Initializes a new Enenmap, with options.
      * @param {Object} [options={}] Additional options for the enenmap.
-     * @param {String} [options.name=""] The name of the enenmap. If present, Enmap marks it as persistant. Represents the table name in sqlite.
+     * @param {String} [options.name=""] The name of the enenmap. If present, Enmap marks it as persistent. Represents the table name in sqlite.
      * @param {Boolean} [options.fetchAll=true] When enabled, will automatically fetch keys that have been requested via `get()` or other retrieval options.
      * @param {String} [options.dataDir="./data"] Determines where the sqlite files will be stored. Can be relative or absolute. **Windows users, __remember to escape your backslashes__!**
-     * @param {String} [options.cloneLevel="deep"] Determines how obects and arrays are treated then inserting and retrieving from the database.
-     * @param {Boolean} [options.polling=false] Determines whether enenmap will try to fetch changes from the Database at a regulat interval. This allows multiple processes to modify the values with ease!
+     * @param {String} [options.cloneLevel="deep"] Determines how objects and arrays are treated then inserting and retrieving from the database.
+     * @param {Boolean} [options.polling=false] Determines whether enenmap will try to fetch changes from the Database at a regular interval. This allows multiple processes to modify the values with ease!
      * @param {String} [options.pollingInterval="1000"] How often (in *milliseconds*) to poll the database. See `options.polling`.
      * @param {Boolean} [options.ensureProps=true] If enabled and the value in the enenmap is an object, using ensure() will also ensure that every property present in the default object will be added to the value, if it's absent.
      * @param {*} [options.autoEnsure] When provided a value, essentially runs ensure(key, autoEnsure) automatically so you don't have to. This is especially useful on get(), but will also apply on set(), and any array and object methods that interact with the database.
@@ -38,27 +38,29 @@ class EnEnmap extends Enmap {
      * // Enenmap that automatically assigns a default object when getting or setting anything.
      * const autoEnmap = new Enenmap({name: "settings", autoEnsure: { setting1: false, message: "default message"}})
      */
-    constructor(options={}) { // No defaults needed, enmap does this.
-        let parsedopts = {};
+    constructor(options = {}) { // No defaults needed, enmap does this.
+        let parsedOpts = {};
         if (options) {
             // eslint-disable-next-line no-inner-declarations
-            function addOpt(name) {
-                if (options[name]) {
-                    eval(`parsedopts = {...parsedopts, ${name}: ${options[name]}}`);
+            [
+                'name',
+                'fetchAll',
+                'dataDir',
+                'cloneLevel',
+                'polling',
+                'pollingInterval',
+                'ensureProps',
+                'autoEnsure',
+                'autoFetch',
+                'wal'
+            ]
+                .forEach(name => {
+                    if (options[name]) {parsedOpts[name] = options[name];}
                 }
-            }
-            addOpt('name');
-            addOpt('fetchAll');
-            addOpt('dataDir');
-            addOpt('cloneLevel');
-            addOpt('polling');
-            addOpt('pollingInterval');
-            addOpt('ensureProps');
-            addOpt('autoEnsure');
-            addOpt('autoFetch');
-            addOpt('wal');
+                );
+
         }
-        super(parsedopts);
+        super(parsedOpts);
     }
 }
 
